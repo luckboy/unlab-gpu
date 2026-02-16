f192bd04055e1a68e41359db837340ede6375c06
Lukasz Szpakowski <luckboy@vp.pl>
2026-02-16 13:01:50 +0100

    Implemented documentation generator.

--- src/doc.rs
+++ src/doc.rs
@@ -5,12 +5,21 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
+use std::fs::File;
+use std::fs::create_dir;
+use std::fs::write;
+use std::io;
+use std::io::BufWriter;
+use std::io::Write;
 use std::path;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::sync::RwLock;
 use std::sync::RwLockReadGuard;
+use markdown::CompileOptions;
+use markdown::ParseOptions;
+use markdown::Options;
 use crate::error::*;
 use crate::mod_node::*;
 use crate::parser::*;
@@ -320,6 +329,235 @@ impl DocTreeGen
                 }
             },
             _ => (),
+        }
+        Ok(())
+    }
+}
+
+fn str_to_hmtl(s: &str) -> String
+{ s.replace('&', "&amp").replace('<', "&lt").replace(">", "&gt") }
+
+fn idents_to_string(idents: &[String]) -> String
+{
+    let mut s = String::new();
+    let mut is_first = true;
+    for ident in idents {
+        if !is_first {
+            s.push_str("::");
+        }
+        s.push_str(ident);
+        is_first = false;
+    }
+    s
+}
+
+#[derive(Clone, Debug)]
+pub struct DocGen
+{
+    lib_name: String,
+    lib_doc_dir: PathBuf,
+}
+
+impl DocGen
+{
+    pub fn new<P: AsRef<Path>>(doc_dir: PathBuf, lib_dir: P) -> Self
+    {
+        let lib_name = lib_dir.as_ref().to_string_lossy().into_owned().replace(path::MAIN_SEPARATOR, "/");
+        let mut lib_doc_dir = doc_dir;
+        lib_doc_dir.push(lib_dir);
+        DocGen { lib_name, lib_doc_dir, }
+    }
+    
+    pub fn lib_name(&self) -> &str
+    { self.lib_name.as_str() }
+
+    pub fn lib_doc_dir(&self) -> &Path
+    { self.lib_doc_dir.as_path() }
+    
+    fn str_to_href(s: &str, depth: usize) -> String
+    {
+        let mut url = String::new();
+        for _ in 0..depth {
+            url.push_str("../");
+        }
+        url.push_str(s);
+        url
+    }
+
+    fn ident_and_sig_to_html(ident: &str, sig: &Sig) -> String
+    {
+        let mut html = String::new();
+        html.push_str("<h3>");
+        html.push_str(format!("<a href=\"#var.{}\" class=\"var\">{}</a>", ident, ident).as_str());
+        match sig {
+            Sig::Var => (),
+            Sig::Fun(args) => {
+                html.push('(');
+                let mut is_first = true;
+                for arg in args {
+                    if !is_first {
+                        html.push_str(", ");
+                    }
+                    html.push_str(format!("<span class=\"arg\">{}</span>", arg).as_str());
+                    is_first = false;
+                }
+                html.push(')');
+            },
+            Sig::BuiltinFun(args) => {
+                html.push('(');
+                let mut is_first = true;
+                for arg in args {
+                    if !is_first {
+                        html.push_str(", ");
+                    }
+                    match arg {
+                        BuiltinFunArg::Arg(ident) => html.push_str(format!("<span class=\"arg\">{}</span>", ident).as_str()),
+                        BuiltinFunArg::OptArg(ident) => html.push_str(format!("<span class=\"arg\">{}</span>?", ident).as_str()),
+                        BuiltinFunArg::DotDotDot => html.push_str("..."),
+                    }
+                    is_first = false;
+                }
+                html.push(')');
+            },
+        }
+        html.push_str("</h3>");
+        html
+    }
+    
+    fn markdown_to_html(s: &str) -> Result<String>
+    {
+        match latex2mathml::replace(s) {
+            Ok(t) => {
+                let options = Options {
+                    parse: ParseOptions::gfm(),
+                    compile: CompileOptions {
+                        allow_dangerous_html: true,
+                        gfm_tagfilter: true,
+                        ..CompileOptions::default()
+                    },
+                    ..Options::default()
+                };
+                match markdown::to_html_with_options(t.as_str(), &options) {
+                    Ok(u) => Ok(u),
+                    Err(msg) => Err(Error::Markdown(format!("{}", msg))),
+                }
+            },
+            Err(err) => Err(Error::Latex2mathml(Box::new(err))),
+        }
+    }
+    
+    fn io_res_generate_html_file<P: AsRef<Path>>(&self, path: P, idents: &[String], content: &str, depth: usize) -> io::Result<()>
+    {
+        let file = File::create(path)?;
+        let mut w = BufWriter::new(file);
+        writeln!(&mut w, "<!DOCTYPE html>")?;
+        writeln!(&mut w, "<html>")?;
+        writeln!(&mut w, "<head>")?;
+        writeln!(&mut w, "<meta charset=\"utf-8\" />")?;
+        writeln!(&mut w, "<link rel=\"stylesheet\" href=\"{}\" />", Self::str_to_href("styles.css", depth))?;
+        write!(&mut w, "<title>")?;
+        if !idents.is_empty() {
+            write!(&mut w, "{} in ", idents_to_string(idents))?;
+        }
+        write!(&mut w, "{} - Unlab", str_to_hmtl(self.lib_name.as_str()))?;
+        writeln!(&mut w, "</title>")?;
+        writeln!(&mut w, "</head>")?;
+        writeln!(&mut w, "<body>")?;
+        writeln!(&mut w, "<header>")?;
+        if !idents.is_empty() {
+            writeln!(&mut w, "<h1><a href=\"{}\">{}</a></h1>", Self::str_to_href("index.html", depth), str_to_hmtl(self.lib_name.as_str()))?;
+        } else {
+            writeln!(&mut w, "<h1>{}</h1>", str_to_hmtl(self.lib_name.as_str()))?;
+        }
+        writeln!(&mut w, "</header>")?;
+        match idents.first() {
+            Some(first_ident) => {
+                writeln!(&mut w, "<nav>")?;
+                let mut mod_path = String::from(first_ident);
+                write!(&mut w, "<h2><a href=\"{}\">{}</a>", Self::str_to_href(format!("{}.html", mod_path).as_str(), depth), first_ident)?;
+                for ident in &idents[1..] {
+                    mod_path.push('/');
+                    mod_path.push_str(ident.as_str());
+                    write!(&mut w, "::")?;
+                    write!(&mut w, "<a href=\"{}\">{}</a>", Self::str_to_href(format!("{}.html", mod_path).as_str(), depth), ident)?;
+                }
+                writeln!(&mut w, "</h2>")?;
+                writeln!(&mut w, "</nav>")?;
+            },
+            None => (),
+        }
+        writeln!(&mut w, "<main>")?;
+        writeln!(&mut w, "{}", content)?;
+        writeln!(&mut w, "</main>")?;
+        writeln!(&mut w, "</body>")?;
+        writeln!(&mut w, "</html>")?;
+        Ok(())
+    }
+
+    fn generate_mod_doc(&self, doc_tree: &DocTree, idents: &[String], path: &Path, mod_path: &str, index_content: &mut String, depth: usize) -> Result<()>
+    {
+        index_content.push_str(format!("<li><a href=\"{}\">{}</a></li>\n", Self::str_to_href(format!("{}.html", mod_path).as_str(), depth), idents_to_string(idents)).as_str());
+        let doc_tree_g = doc_tree.read()?;
+        let mut subtrees = doc_tree_g.subtrees();
+        if !subtrees.is_empty() {
+            match create_dir(path) {
+                Ok(()) => (),
+                Err(err) => return Err(Error::Io(err)),
+            }
+            subtrees.sort_by(|p1, p2| p1.0.cmp(p2.0));
+            for (ident, subtree) in subtrees {
+                let mut new_idents = idents.to_vec();
+                new_idents.push(ident.clone());
+                let mut new_path = PathBuf::from(path);
+                new_path.push(ident);
+                let mut new_mod_path = String::from(mod_path);
+                new_mod_path.push('/');
+                new_mod_path.push_str(ident);
+                self.generate_mod_doc(&subtree, new_idents.as_slice(), new_path.as_path(), new_mod_path.as_str(), index_content, depth + 1)?;
+            }
+        }
+        let mut mod_content = String::new();
+        mod_content.push_str("<section>\n");
+        match doc_tree_g.desc() {
+            Some(desc) => mod_content.push_str(format!("{}\n", Self::markdown_to_html(desc.as_str())?).as_str()),
+            None => (),
+        }
+        mod_content.push_str("</section>\n");
+        let mut var_desc_pairs = doc_tree_g.var_desc_pairs();
+        var_desc_pairs.sort_by(|p1, p2| p1.0.cmp(p2.0));
+        for (ident, (sig, desc))  in var_desc_pairs {
+            mod_content.push_str(format!("<section id=\"var.{}\">\n", ident).as_str());
+            mod_content.push_str(Self::ident_and_sig_to_html(ident, sig).as_str());
+            match desc {
+                Some(desc) => mod_content.push_str(format!("{}\n", Self::markdown_to_html(desc.as_str())?).as_str()),
+                None => (),
+            }
+            mod_content.push_str("</section>\n");
+        }
+        match self.io_res_generate_html_file(path.with_extension(".html"), idents, mod_content.as_str(), depth) {
+            Ok(()) => (),
+            Err(err) => return Err(Error::Io(err)),
+        }
+        Ok(())
+    }
+    
+    pub fn generate(&self, doc_tree: &DocTree) -> Result<()>
+    {
+        let mut styles_path_buf = self.lib_doc_dir.clone();
+        styles_path_buf.push("styles.css");
+        match write(styles_path_buf, "") {
+            Ok(()) => (),
+            Err(err) => return Err(Error::Io(err)),
+        }
+        let mut path_buf = self.lib_doc_dir.clone();
+        path_buf.push("root");
+        let mut index_content = String::new();
+        self.generate_mod_doc(doc_tree, &[String::from("root")], path_buf.as_path(), "root", &mut index_content, 0)?;
+        let mut index_path_buf = self.lib_doc_dir.clone();
+        index_path_buf.push("index.html");
+        match self.io_res_generate_html_file(index_path_buf, &[], index_content.as_str(), 0) {
+            Ok(()) => (),
+            Err(err) => return Err(Error::Io(err)),
         }
         Ok(())
     }
