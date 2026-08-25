3ec9a48ca165dc5692d9b8b61eb95409a78abeee
Lukasz Szpakowski <luckboy@vp.pl>
2026-08-25 12:32:11 +0200

    Fixed.

--- src/value.rs
+++ src/value.rs
@@ -195,7 +195,7 @@ impl Value
         match self {
             Value::Object(object) => {
                 match &**object {
-                    Object::Fun(_, _, _) | Object::BuiltinFun(_, _) => true,
+                    Object::Fun(_, _, _) | Object::BuiltinFun(_, _) | Object::UnnamedFun(_, _) => true,
                     _ => false,
                 }
             },
