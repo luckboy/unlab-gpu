3e1f71cb19109fe3de68322b943578124521da90
Lukasz Szpakowski <luckboy@vp.pl>
2026-09-18 13:52:12 +0200

    Fixed test.

--- src/builtins/tests.rs
+++ src/builtins/tests.rs
@@ -911,6 +911,11 @@ fn test_length_is_applied_with_success()
                 Ok(value) => assert_eq!(Value::Int(3), value),
                 Err(_) => assert!(false),
             }
+            let arg_value = Value::Object(Arc::new(Object::String(String::from("ąbć"))));
+            match fun_value.apply(&mut interp, &mut env, &[arg_value]) {
+                Ok(value) => assert_eq!(Value::Int(3), value),
+                Err(_) => assert!(false),
+            }
             let a = vec![
                 1.0, 2.0,
                 3.0, 4.0,
